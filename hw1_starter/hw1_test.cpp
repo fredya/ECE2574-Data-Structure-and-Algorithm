ab0c3ea677ada6b4171613251432c1b2015e5621
Yazhe_fred <yazhehu@vt.edu>
2018-02-08 12:46:11 -0500

    Hw1 original files

---
+++ hw1_starter/hw1_test.cpp
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////
+// ECE 2574, Homework 1, starter code
+//
+#define CATCH_CONFIG_MAIN			// this line tells Catch to provide a main() function
+#define CATCH_CONFIG_COLOUR_NONE	// this line avoids problems due to color-coding the output
+#include "catch.hpp"
+
+#include "ArrayBag.h" 
+
+// The following tests need to be expanded, as described in the specification
+TEST_CASE("Test the 'replace' method", "[ArrayBag]") {
+	
+	ArrayBag<double> b;
+	REQUIRE(b.isEmpty());	
+}
